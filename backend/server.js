4be026066ea02cbc95d40f823fc713c8561d463c
Anto Alex <82249465+antoalex07@users.noreply.github.com>
2023-03-22 15:55:32 +0530

    CORS frontend access

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,12 @@ app.use(cors({
 }));
 app.use("/api/tasks", taskRoute);
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://task-manager-app-my06.onrender.com");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 
 // create Routes
 
